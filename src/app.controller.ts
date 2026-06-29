50832d37d4f0ef2a6c006425f1a4f4d9d7d37731
usmankhan4001 <ranausmankhan4001@gmail.com>
2026-06-29 17:00:45 +0500

    fix: remove root hello route to allow react static serving

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/api/ping')
   getHello(): string {
     return this.appService.getHello();
   }
